3205d1e6b202fe277f10cdabb9944c64bc2e5749
Jonatas Braz <jonatasbraz@gmail.com>
2020-05-22 15:38:24 -0300

    upload de imagens / atualizar avatar, excluir arquivo lixo

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,13 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
 export default {
+  directory: uploadFolder,
+
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    destination: uploadFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
