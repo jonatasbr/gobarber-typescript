97bc5ece57ca83cd188689259a798197f3d6ef97
Jonatas Braz <jonatasbraz@gmail.com>
2020-05-22 17:45:38 -0300

    upload de imagens / servir arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import 'reflect-metadata';
 import express from 'express';
 
 import routes from './routes';
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Node API started on port 3333');
+  console.log('Node API started on port 3333!');
 });
 
 export default app;
