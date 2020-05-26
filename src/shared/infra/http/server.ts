efc75e7c306cf7286d20db4697ccd54ba75abac1
Jonatas Braz <jonatasbraz@gmail.com>
2020-05-26 11:48:46 -0300

    arquitetura ddd / injecao de dependencias

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,12 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
-import '@shared/infra/typeorm';
-
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
 import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
