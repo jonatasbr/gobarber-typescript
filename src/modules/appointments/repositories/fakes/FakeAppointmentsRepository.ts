9ebcfef64ede0b601ea1bbd992c36aff2d56825a
Jonatas Braz <jonatasbraz@gmail.com>
2020-05-27 18:26:03 -0300

    testes e tdd / criando o primeiro teste - teste unitario de create appointment

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
