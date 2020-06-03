c0aab4b6f774e35a80a4be6013c074ac48afc715
Jonatas Braz <jonatasbraz@gmail.com>
2020-06-03 02:51:23 -0300

    continuando back-end - reset senha e finalizando testes

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -3,7 +3,7 @@ import IUserTokensRepository from '@modules/users/repositories/IUserTokensReposi
 import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
 
 class FakeUserTokenRepository implements IUserTokensRepository {
-  private userToken: UserToken[] = [];
+  private userTokens: UserToken[] = [];
 
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = new UserToken();
@@ -11,10 +11,18 @@ class FakeUserTokenRepository implements IUserTokensRepository {
     Object.assign(userToken, {
       id: uuid(),
       token: uuid(),
-      user_id
+      user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
-    this.userToken.push(userToken);
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(findToken => findToken.token === token);
 
     return userToken;
   }
